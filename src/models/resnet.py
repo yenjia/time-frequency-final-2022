533cb2e12f6e5f7d040565ee76e1babcf3d9840f
yenjia <rex19981002@gmail.com>
2022-12-27 14:29:35 +0800

    remove init (combine into the resnet)

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -1,43 +1,5 @@
-# This code is built from the PyTorch examples repository: https://github.com/pytorch/vision/tree/master/torchvision/models.
-# Copyright (c) 2017 Torch Contributors.
-# The Pytorch examples are available under the BSD 3-Clause License.
-#
-# ==========================================================================================
-#
-# Adobe’s modifications are Copyright 2019 Adobe. All rights reserved.
-# Adobe’s modifications are licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
-# 4.0 International Public License (CC-NC-SA-4.0). To view a copy of the license, visit
-# https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.
-#
-# ==========================================================================================
-#
-# BSD-3 License
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# * Neither the name of the copyright holder nor the names of its
-#   contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-
-from models_dwt import *
+import torch.nn as nn
+from DWT_IDWT.DWT_IDWT_layer import *
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d']
@@ -50,6 +12,15 @@ model_urls = {
      'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
      'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
  }
+
+class Downsample(nn.Module):
+    def __init__(self, wavename = 'haar'):
+        super(Downsample, self).__init__()
+        self.dwt = DWT_2D_tiny(wavename = wavename)
+
+    def forward(self, input):
+        LL = self.dwt(input)
+        return LL
 
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1):
