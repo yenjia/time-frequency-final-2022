2ebf01ffcf5a2edfe10d0194954e24a280dd2fbe
yenjia <rex19981002@gmail.com>
2022-12-27 14:19:10 +0800

    remove 1D (only keep the 2D version)

--- src/models/DWT_IDWT/DWT_IDWT_Functions.py
+++ src/models/DWT_IDWT/DWT_IDWT_Functions.py
@@ -1,34 +1,6 @@
 import torch
 from torch.autograd import Function
 
-class DWTFunction_1D(Function):
-    @staticmethod
-    def forward(ctx, input, matrix_Low, matrix_High):
-        ctx.save_for_backward(matrix_Low, matrix_High)
-        L = torch.matmul(input, matrix_Low.t())
-        H = torch.matmul(input, matrix_High.t())
-        return L, H
-    @staticmethod
-    def backward(ctx, grad_L, grad_H):
-        matrix_L, matrix_H = ctx.saved_variables
-        grad_input = torch.add(torch.matmul(grad_L, matrix_L), torch.matmul(grad_H, matrix_H))
-        return grad_input, None, None
-
-
-class IDWTFunction_1D(Function):
-    @staticmethod
-    def forward(ctx, input_L, input_H, matrix_L, matrix_H):
-        ctx.save_for_backward(matrix_L, matrix_H)
-        output = torch.add(torch.matmul(input_L, matrix_L), torch.matmul(input_H, matrix_H))
-        return output
-    @staticmethod
-    def backward(ctx, grad_output):
-        matrix_L, matrix_H = ctx.saved_variables
-        grad_L = torch.matmul(grad_output, matrix_L.t())
-        grad_H = torch.matmul(grad_output, matrix_H.t())
-        return grad_L, grad_H, None, None
-
-
 class DWTFunction_2D(Function):
     @staticmethod
     def forward(ctx, input, matrix_Low_0, matrix_Low_1, matrix_High_0, matrix_High_1):
