6116a6aa7e7758b81b167c81113717c3cfd32b07
yenjia <rex19981002@gmail.com>
2022-12-28 15:42:52 +0800

    add 'noise' transform

--- src/transforms.py
+++ src/transforms.py
@@ -1,4 +1,5 @@
 from math import pi
+from skimage.util import random_noise
 import torch
 import torch.nn.functional as F
 from torchvision.io import read_image, ImageReadMode
@@ -69,6 +70,34 @@ class AutoAugmentd(BaseTransform):
     def _process(self, single_data, **kwargs):
         return self.autoaug(single_data)
     
+class Noised(BaseTransform):
+    '''
+    This transformation supports two types of noise: Gaussian and Impulse noise 
+    (Salt and Pepper noise). The parameter "severty" is the same as 
+    https://github.com/hendrycks/robustness, which applies several "corruptions"
+    to create the ImageNet-C.
+    Args:
+        mode: the noise mode, defaults to "gaussian"
+        serverty: number from 0 to 4, defaults to 0
+        guassian: predefined serverty
+        impluse: predifined serverty
+    '''
+    def __init__(self, keys, mode="gaussian", severty=0, **kwargs):
+        super(Noised, self).__init__(keys, **kwargs)
+        self.mode = mode
+        self.severty = severty
+        self.gaussian = [0.08, 0.12, 0.18, 0.26, 0.38]
+        self.impluse = [0.03, 0.06, 0.09, 0.17, 0.27]
+    
+    def _process(self, single_data, **kwargs):
+        if self.mode == "gaussian":
+            img = single_data + torch.randn(single_data.size())*self.gaussian[self.severty]
+            return torch.clip(img, 0, 1)
+        elif self.mode == "s&p":
+            img = random_noise(single_data, mode=self.mode, amount=self.impluse[self.severty])
+            return torch.from_numpy(img)
+
+
 def train_transforms(**transforms_config):
     img_size = transforms_config.get("size", 224)
     
@@ -80,8 +109,10 @@ def train_transforms(**transforms_config):
         transforms += [AutoAugmentd(keys=["image"])]
     transforms += [
         Scale01d(keys=["image"]),
-        Normalized(keys=["image"]),
+        # Normalized(keys=["image"]),
     ]
+    if "noise" in transforms_config and isinstance(transforms_config["noise"], dict):
+        transforms += [Noised(keys=["image"], **transforms_config["noise"])]
 
     return Compose(transforms)
 
@@ -91,7 +122,9 @@ def val_transforms(**transforms_config):
         LoadImaged(keys=["image"]),
         Resized(keys=["image"], size=(img_size, img_size)),
         Scale01d(keys=["image"]),
-        Normalized(keys=["image"]),
+        # Normalized(keys=["image"]),
     ]
+    if "noise" in transforms_config and isinstance(transforms_config["noise"], dict):
+        transforms += [Noised(keys=["image"], **transforms_config["noise"])]
 
     return Compose(transforms)
